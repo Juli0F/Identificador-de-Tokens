932630e57e54ea9e3b5bd7906d40d4b10f1eaf09
Julio_Ub <juliofernando-ixcou@cunoc.edu.gt>
2020-08-19 23:37:08 -0600

    dos archivos mas

---
+++ PracticaCorta/PracticaCorta/MainWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Gtk;
+using PracticaCorta.Identify;
+using System.Collections.Generic;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void btnAction(object sender, EventArgs e)
+    {
+
+
+        Identify identificar = new Identify(txtStatement.Text);
+        identificar.applySplit();
+
+
+        List<List<String>> listas = new List<List<string>>();
+
+        listas.Add(identificar.GetListEnteros());
+        listas.Add(identificar.GetListDecimaless());
+        listas.Add(identificar.GetListPalabras());
+        listas.Add(identificar.GetListMoneda());
+
+        TreeViewExample verTokens  = new TreeViewExample(listas);
+        //verTokens.addItem("test", "Correcto");
+        verTokens.windowShow();
+
+        //verTokens.addItem("Numerico", "test");
+
+        //verTokens.addList("Enteros", identificar.GetListEnteros());
+        //verTokens.addList("Decimales", identificar.GetListDecimaless());
+        //verTokens.addList("Palabras", identificar.GetListPalabras());
+        //verTokens.addList("Moneda", identificar.GetListMoneda());
+
+    }
+
+}
